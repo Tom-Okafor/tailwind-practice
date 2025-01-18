aec7ca8218b51b6b15d4f6caf712f2557544295b
Tom Okafor <tmeka008@gmail.com>
2025-01-18 19:56:46 +0100

    style: practice styling using tailwind.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ function App() {
   return (
     <>
       <h1 className="text-5xl px-4 text-purple-500">Hello Universe.</h1>
-      <div className="*:bg-red-800 *:w-[100px] *:h-[100px] *:rounded-3xl  flex gap-6 mx-4 my-6">
+      <div className="*:bg-red-800 *:w-[100px] *:h-[100px] *:rounded-3xl  flex gap-6 mx-4 my-6 ">
         <div></div>
         <div></div>
         <div></div>
       </div>
+      <section className="bg-teal-200 h-[60vh] flex gap-8">
+        <article className="article"></article>
+        <article className="article"></article>
+        <article className="article"></article>
+      </section>
     </>
   );
 }
