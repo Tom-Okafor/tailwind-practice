b34ca9d2874493e9f93a43e9eeacb4c389d9508c
Tom Okafor <tmeka008@gmail.com>
2025-01-21 23:13:14 +0100

    feat(ui): add unstyled profile cards with random details.

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,50 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-5xl px-4 text-purple-500">Hello Universe.</h1>
-      <div className="*:bg-red-800 *:w-[100px] *:h-[100px] *:rounded-3xl  flex gap-6 mx-4 my-6 ">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <section className="bg-teal-200 h-[60vh] flex gap-8">
-        <article className="article"></article>
-        <article className="article"></article>
-        <article className="article"></article>
+      <h1 className="text-5xl px-4 text-purple-500">Unity Corporations.</h1>
+      <h2>Staff Leads.</h2>
+      <section>
+        <div>
+          <img
+            src="https://th.bing.com/th?id=OIP.u-Yt2tcCjepBr-6TB7-EegHaJQ&w=223&h=279&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+            alt=""
+          />
+          <h3>Aubery Hamford</h3>
+          <h4>Data Analyst</h4>
+          <p>
+            Aubery Hamford is the chief data analyst of Unity Corporations. He
+            is a genius at handling data and optimizing the most random sets for
+            optimum performance.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://th.bing.com/th?id=OIP.sZ4wMoVgQYmItcIiNLceuQHaLH&w=204&h=306&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+            alt=""
+          />
+          <h3>Sarah Pinnock</h3>
+          <h4>Research Manager</h4>
+          <p>
+            Sarah Pinnock is the chief Research Manager at Unity Corporation.
+            She is a pioneer staff of the company and has worked tirelessly to
+            ensure accurate information and excellent results.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://th.bing.com/th/id/OIP.28lI-o8NNikT4vWt3T-GVAHaLH?w=184&h=276&c=7&r=0&o=5&pid=1.7"
+            alt=""
+          />
+          <h3>Fatima Rosebudding</h3>
+          <h4>Communications Manager</h4>
+          <p>
+            Fatima Rosebudding is another pioneer staff of Unity Corporation.
+            She is the lead communciations manager and ensures that there are no
+            information leaks or communciation gaps. Everyone at Unity
+            Corporations is abreast of all the happenings and events because of
+            Ms. Fatima!
+          </p>
+        </div>
       </section>
     </>
   );
